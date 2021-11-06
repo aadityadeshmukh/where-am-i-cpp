7305b83e49338734a9bd952e8f111e40a0662cdd
Aaditya Deshmukh <aadityadeshmukh@gmail.com>
2021-11-06 08:59:08 +0000

    Added windows API which is incompatible with replit

--- GLWifiPoints.cpp
+++ GLWifiPoints.cpp
@@ -1,6 +1,10 @@
 #include "GLWifiPoints.hpp"
+using namespace std;
 
 int GLWifiPoints::getWifiNetworks(){
 	cout << "Fetching Wifi access points." << endl;
+
+	//open handle
+	DWORD dwClientVer = 2;
 	return 0;
 }
